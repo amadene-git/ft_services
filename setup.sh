281cb6bd165e2aa10b3b1a6f7e656e4c593e9841
amadene-git <adioumd@hotmail.fr>
2021-04-03 18:01:08 +0200

    lol.exe

---
+++ setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+minikube stop
+minikube delete
+minikube start --vm-driver=docker
+
+
+IP_EXT= $(minikube ip)
+
+#MetalLB
+
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl apply -f - -n kube-system
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
+cat <<EOF | kubectl create -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  namespace: metallb-system
+  name: config
+data:
+  config: |
+    address-pools:
+    - name: default
+      protocol: layer2
+      addresses:
+      - $IP_EXT-192.168.49.42
+EOF
+
+#kubectl apply -f nginx/nginx.yaml
