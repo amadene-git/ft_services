49341a097a5f166a26ca80ddcf1be8ac89bd04d8
amadene-git <adioumd@hotmail.fr>
2021-04-21 18:00:15 +0200

    nginx https

--- srcs/mysql/start.sh
+++ srcs/mysql/start.sh
@@ -1,4 +1,21 @@
-#!bin/sh
+#!/bin/sh
 
 
-tail -f /dev/null
+mkdir -p /run/nginx
+mkdir -p /var/www/html
+
+openssl req -x509 -nodes -days 365 \
+	-subj "/C=CA/ST=QC/O=Company, Inc./CN=mydomain.com" \
+	-newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key \
+	-out /etc/ssl/certs/nginx-selfsigned.crt;
+
+
+echo "salut" > /var/www/html/index.html
+
+#nginx
+
+#sh
+#tail -f /dev/null
+
+
+nginx -g 'daemon off;'
